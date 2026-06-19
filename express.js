ce9a44a7caa80202415c6b802298f778c8f0c129
jmonty82 <jmontrose82@gmail.com>
2026-06-19 18:22:06 -0400

    Added POST, PATCH, PUT, and DELETE routes

--- express.js
+++ express.js
@@ -5,22 +5,51 @@ import charactersRoutes from "./routes/characters.js"
 import imagesRoutes from "./routes/images.js"
 
 
-// create our express app
+// Create our express app
 const app = express();
 
-// set up a port
+// Set up a port
 const port = 3000
+
+
 
 app.use("/anime", animeRoutes)
 app.use("/characters", charactersRoutes)
 app.use("/images", imagesRoutes)
 
-// create a route
+
+
+// Create a route
 app.get("/", (req, res) => {
-res.send("Hello World");
+    res.send("Hello World");
 })
 
-//starting our server = bottom of page
+const items = {
+    "id": "",
+    "name": "",
+    "summary": ""
+}
+// Create a POST route
+app.post("/", (req, res) => {
+    res.send("New Post Added!")
+    items.push(req.body);
+    res.status(201).json(item);
+});
+
+// Create PATCH or PUT routes for data
+app.patch("/", (req, res) => {
+    res.send("Post Updated!")
+})
+app.put("/", (req, res) => {
+    res.send("Post Changed!")
+})
+
+// Create DELETE routes for data
+app.delete("/", (req, res) => {
+    res.send("Post Deleted!")
+})
+
+//Starting our server = bottom of page
 app.listen(port, () => {
     console.log("Listening on port: " + port)
 })
--- routes/anime.js
+++ routes/anime.js
@@ -7,7 +7,12 @@ router.get("/", (req, res) => {
     res.send(animeData)
 })
 
-
+// create a POST route
+router.post("/", (req, res) => {
+    res.send(animeData)
+    items.push(req.body);
+    res.status(201).json(item);
+});
 
 
 
