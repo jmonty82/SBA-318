61f49ba903633d493a53fc6421f62cdcc27778e8
jmonty82 <jmontrose82@gmail.com>
2026-06-24 23:15:41 -0400

    Rendered form from Views file

--- express.js
+++ express.js
@@ -4,6 +4,8 @@ import animeRoutes from "./routes/anime.js"
 import charactersRoutes from "./routes/characters.js"
 import imagesRoutes from "./routes/images.js"
 import animeData from "./data/anime.js";
+import ejs from "ejs"
+
 
 
 // Create our express app
@@ -13,11 +15,29 @@ const app = express();
 const port = 3000
 app.use(express.json())
 
+// Views template and engine
+app.set("view engine", "ejs")
+app.get("/", (req, res) => {
+    res.render("form.ejs")
+})
 
 app.use("/anime", animeRoutes)
 app.use("/characters", charactersRoutes)
 app.use("/images", imagesRoutes)
 
+
+// add the middleware to our app
+app.use((req, res, next) => {
+    console.log(req.method, req.url);
+    // take us to the next middleware or the next route
+    next()
+})
+
+// error handling middleware
+app.use((err, req, res, next) => {
+    console.log(err.message)
+    res.status(500).send("something went wrong")
+})
 
 
 // Create a route
@@ -41,18 +61,38 @@ app.post("/", (request, response) => {
     response.status(201).send(item);
 });
 
+const newCharacter =
+{
+    "id": 9,
+    "firstname": "Orihime",
+    "lastname": "Inoue",
+    "series": "Bleach"
+}
 // Create PATCH or PUT routes for data
 app.patch("/", (req, res) => {
-    res.send("Post Updated!")
-})
+    const id = parseInt(req.params.id);
+    const updatedItem = req.body;
+    items[id] = updatedItem;
+    res.send(updatedItem);
+});
+
 app.put("/", (req, res) => {
     res.send("Post Changed!")
 })
 
+// query parameters (api-key, limit, breed-id) // cat.api.com/api/?limit=10 // (filter) (tracking) (optional)
+app.get("/my*cat", (req, res) => {
+    console.log(req.query)
+res.send("Meow!!")
+})
+
 // Create DELETE routes for data
 app.delete("/", (req, res) => {
-    res.send("Post Deleted!")
-})
+    // res.send("Post Deleted!")
+    const id = parseInt(req.params.id);
+    items.splice(id, 1);
+    res.status(204).send();
+});
 
 //Starting our server = bottom of page
 app.listen(port, () => {
